66c747ddb7be072d915bc0bd29acdd6e426f1307
crj1998 <crj1998@sjtu.edu.cn>
2022-10-13 12:27:48 +0000

    add GPR

--- init.sh
+++ init.sh
@@ -1,1 +1,6 @@
 pip install numpy matplotlib pandas
+pip install scipy
+pip install scikit-learn
+git config --global user.email "crj1998@sjtu.edu.cn"
+git config --global user.name "crj1998"
+# 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
