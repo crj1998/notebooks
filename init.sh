a622c5b67b1e4e1e15b14c4104915e1db35536b9
crj1998 <crj1998@sjtu.edu.cn>
2022-10-13 16:04:07 +0000

    gpr

--- init.sh
+++ init.sh
@@ -3,4 +3,3 @@ pip install scipy
 pip install scikit-learn
 git config --global user.email "crj1998@sjtu.edu.cn"
 git config --global user.name "crj1998"
-# ghp_XEw0fcWfFY94lfXXqn9QbKBEyUsivz334i ++ 3D
